df36187677690530f9711ccf3875eb2dc51d3e90
Selvamani Andiappan <127743694+selvamani1992@users.noreply.github.com>
2023-09-14 17:01:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import streamlit as st
+from PIL import Image
 
+
+st.set_page_config(page_title="Selvamani Andiappan",
+                   page_icon="",
+                   layout="wide",
+                   initial_sidebar_state="expanded")
+
+# Define the CSS style
+background_style = """
+<style>
+body {
+    background-image: url('Background.png');
+    background-size: cover;
+}
+</style>
+"""
+
+# Display the CSS style using st.markdown
+st.markdown(background_style, unsafe_allow_html=True)
+
+st.title("Selvamani Andiappan")
 st.title("Welcome to My Webpage")
 st.image('sample.jpg')
 st.write('Thank You')
